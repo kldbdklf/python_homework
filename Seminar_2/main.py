b901c42e7c1ba42ea7ef672056382ac0b99bfe0e
Ivan <ivanchul@yandex.ru>
2023-07-18 19:32:23 +0400

    Solved Task 3

--- Seminar_2/main.py
+++ Seminar_2/main.py
@@ -31,13 +31,13 @@ Task 2:
 Для этого Петя делает две подсказки. Он называет сумму этих чисел S и их произведение P. 
 Помогите Кате отгадать задуманные Петей числа.
 """
-
-print("Task 2 \"Coins\": ")
-x = int(input("Enter first number = "))
-y = int(input("Enter second number = "))
-if x <= 1000 and y <= 1000:
-    s = x + y
-    p = x * y
+"""
+print("Task 2 \"Two numbers\": ")
+first_number = int(input("Enter first number = "))
+second_number = int(input("Enter second number = "))
+if first_number <= 1000 and second_number <= 1000:
+    s = first_number + second_number
+    p = first_number * second_number
     i = 1
     anti_duplication = 0
     while i < s and anti_duplication == 0:
@@ -47,3 +47,21 @@ if x <= 1000 and y <= 1000:
         i += 1
 else:
     print("You are a little crook!")
+"""
+
+"""
+Task 3: 
+Задача 14: Требуется вывести все целые степени двойки (т.е. числа вида 2k), не превосходящие числа N.
+"""
+print("Task 3 \"Degrees of 2\": ")
+input_number = float(input("Enter your number = "))
+if input_number >= 1:
+    print("Degrees: ")
+    degrees_of_two = 0
+    while 2**degrees_of_two <= input_number:
+        print(degrees_of_two, end = ' ')
+        degrees_of_two += 1
+else:
+    print("There are no such powers, when raised to which the number 2, "
+          "the result would be less than the entered number. "
+          "There are no solutions!")
