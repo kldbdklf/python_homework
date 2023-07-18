c11cb50eedc675bee2e9a22b5d43e70166ba9cd9
Ivan <ivanchul@yandex.ru>
2023-07-18 20:02:24 +0400

    Resolved Task 1

--- Seminar_2/main.py
+++ Seminar_2/main.py
@@ -10,19 +10,21 @@ import random
 
 print("Task 1 \"Coins\": ")
 number_of_coins = int(input("Enter number of coins: "))
-temp = 0
-eagles = 0
-for i in range(number_of_coins):
-    temp = random.randint(0,1)
-    # print(temp, end = ' ') для проверки
-    if temp == 1:
-        eagles += 1
-print()
-if (number_of_coins - eagles > eagles):
-    print(f"You need to turn over {eagles} coins!")
+if number_of_coins > 0:
+    temp = 0
+    eagles = 0
+    for i in range(number_of_coins):
+        temp = random.randint(0,1)
+        # print(temp, end = ' ') для проверки
+        if temp == 1:
+            eagles += 1
+    print()
+    if (number_of_coins - eagles > eagles):
+        print(f"You need to turn over {eagles} coins!")
+    else:
+        print(f"You need to turn over {number_of_coins - eagles} coins!")
 else:
-    print(f"You need to turn over {number_of_coins - eagles} coins!")
-
+    print("There should be at least one coin on the table")
 
 """
 Task 2: 
