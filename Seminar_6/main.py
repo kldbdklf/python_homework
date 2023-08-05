e5e9454c786da25728387475c91ac6bb639beea2
Ivan <ivanchul@yandex.ru>
2023-08-05 12:48:44 +0400

    Homework completed

--- Seminar_6/main.py
+++ Seminar_6/main.py
@@ -4,14 +4,14 @@ import random
 # Её первый элемент, разность и количество элементов нужно ввести с клавиатуры.
 # Формула для получения n-го члена прогрессии: an = a1 + (n-1) * d.
 #
-# print("Task 1 Arithmetic progression:")
-# a = int(input('Enter first element (a): '))
-# n = int(input('Enter amount of numbers (n): '))
-# d = int(input('Enter difference (d): '))
-# list_res = []
-# for i in range (n):
-#     list_res.append(a + (i) * d)
-# print (list_res)
+print("Task 1 Arithmetic progression:")
+a = int(input('Enter first element (a): '))
+n = int(input('Enter amount of numbers (n): '))
+d = int(input('Enter difference (d): '))
+list_res = []
+for i in range (n):
+    list_res.append(a + (i) * d)
+print (list_res)
 
 # Task 2:
 # Задача 32: Определить индексы элементов массива (списка), значения которых принадлежат заданному диапазону
